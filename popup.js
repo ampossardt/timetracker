0b12fbe189a112995c770411c7b926b4dfdc534a
DESKTOP-50QDECH\Andrew <andrewmpossardt@gmail.com>
2017-05-10 00:14:19 -0400

    Added project fetching logic

--- popup.js
+++ popup.js
@@ -13,17 +13,104 @@ var app = {
 		}
 
 		chrome.storage.local.set({ "clientList" : clientList });
+		hideOverlay();
+		this.app.updateClientMarkup(clientList);
+	},
+	updateClientMarkup : function(clients) {
+		let body = document.getElementById('clientTableBody');
+
+		for(var i=0; i < clients.length; i++) {
+			let row = document.createElement('tr');
+			let id = document.createElement('td');
+			id.innerText = clients[i].id;
+			let name = document.createElement('td');
+			name.innerText = clients[i].name;
+
+			row.appendChild(id);
+			row.appendChild(name);
+			body.appendChild(row);
+		}
+	},
+	processProject : function(response) {
+		let projects = JSON.parse(response);
+		if(projects === null) return {};
+		let projectList = [];
+
+		for(let i=0; i < projects.length; i++) {
+			let project = {
+				"id" : projects[i].id,
+				"name" : projects[i].name,
+				"color" : projects[i].hex_color
+			};
+			projectList.push(project);
+		}
+		return { "clientId" : projects[0].cid, "projects" : projectList };
+	},
+	updateProjectMarkup : function(projects, clients) {
+		let body = element('#projectTableBody');
+		let sortedClients = clients.sort(function(a,b) {
+			var first = a.name.toLowerCase(), second = b.name.toLowerCase();
+			if(first < second) return -1;
+			else if (first > second) return 1;
+			return 0;
+		});
+
+		sortedClients.forEach(function(client) {
+			var header = document.createElement('h3');
+			header.innerText = client.name;
+			body.appendChild(header);
+
+			var clientProjects = projects.filter(function(project) {
+				return project.clientId === client.id;
+			});
+			let row = document.createElement('tr');
+
+			clientProjects.forEach(function(project) {
+				var id = document.createElement('td');
+				id.innerText = project.id;
+
+				var name = document.createElement('td');
+				name.innerText = project.name;
+
+				row.appendChild(id);
+				row.appendChild(name)
+			});
+
+			body.appendChild(row);
+		});
 	},
 	makeRequest : function(method, url, processCallback) {
 		let request = new XMLHttpRequest();
 		request.open(method, url);
-		request.setRequestHeader("Authorization", getAuthenticationToken());
+		request.setRequestHeader("Authorization", this.getAuthenticationToken());
 		request.onload = function() {
 			if(this.status >= 200 && this.status < 300) {
 				processCallback(request.response);
+			} else {
+				console.log("something broke");
 			}
 		}
 		request.send();
+	},
+	makeRequestWithPromise : function(method, url) {
+		return new Promise(function(success, failure) {
+			let request = new XMLHttpRequest();
+			request.open(method, url);
+			request.setRequestHeader("Authorization", this.app.getAuthenticationToken());
+			request.onload = function() {
+				if(this.status >= 200 && this.status < 300) {
+					success(request.response);
+				} else {
+					console.log("something broke");
+					failure();
+				}
+			}
+			request.send();
+		});
+	},
+	getAuthenticationToken : function () {
+		let key = "5d2497ff82e41340ff6e20c4222ea428";
+		return "Basic " + btoa(key + ":api_token");
 	}
 }
 
@@ -35,6 +122,12 @@ if ( document.addEventListener ) {
 }
 
 function domReady() {
+	initTabs();
+	initManualClientSync();
+	initManualProjectSync();
+}
+
+function initTabs() {
 	var tabs = document.getElementsByClassName('tab');
 	var content = document.getElementsByClassName('content');
 
@@ -51,13 +144,55 @@ function domReady() {
 			addClass(document.getElementById(id), 'active');
 		});
 	}
-
-	app.makeRequest('GET', 'https://www.toggl.com/api/v8/clients', app.processAndStoreClients);
 }
 
-function getAuthenticationToken() {
-	let key = "5d2497ff82e41340ff6e20c4222ea428";
-	return "Basic " + btoa(key + ":api_token");
+function initManualClientSync() {
+	let button = document.getElementById('updateClients');
+	let spinner = document.getElementById('overlay');
+
+	button.addEventListener('click', function() {
+		show(spinner);
+		app.makeRequest('GET', 'https://www.toggl.com/api/v8/clients', app.processAndStoreClients);
+	});
+}
+
+function initManualProjectSync() {
+	let button = element("#updateProjects");
+	let spinner = element("#overlay");
+
+	button.addEventListener('click', function() {
+		show(spinner);
+		chrome.storage.local.get("clientList", function(data) {
+			if(data.clientList === undefined) {
+				console.log("haven't got the client list yet");
+				return;
+			}
+
+			let clients = data.clientList;
+			let url = "https://www.toggl.com/api/v8/clients/{0}/projects";
+			var promises = [];
+			let results = [];
+
+			clients.forEach(function(client) {
+				promises.push(app.makeRequestWithPromise('GET', url.replace('{0}', client.id))
+					.then(function(data){
+						results.push(app.processProject(data));
+				}));
+			});
+
+			Promise.all(promises).then(function(values) {
+				app.updateProjectMarkup(results, clients);
+			});
+		});
+	});
+}
+
+// Utility Functions
+
+
+
+function hideOverlay() {
+	hide(document.getElementById('overlay'));
 }
 
 //
@@ -235,10 +370,6 @@ function getAuthenticationToken() {
 // 	chrome.storage.local.set({ "timeItems" : items });
 // }
 //
-// // fuck this API. you have to get the client name and project name separately because they only
-// // give you the IDs for a time entry, but then they throttle your requests to one per second.
-// // additionally, you can get all of the clients for a person, but you can't get all of their projects.
-// // what the actual fuck
 // function getAdditionalData(items) {
 // 	var token = window.apiKey + ":api_token";
 // 	var request = new XMLHttpRequest();
